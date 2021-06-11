d21f87e9c09473f058165b6b48e3b3d824bc859c
Chayapat Niropas <papae_peez02@hotmail.co.th>
2021-06-11 17:40:33 +0700

    11/06/2021

---
+++ member/sale-page/modal/add_content.php
@@ -0,0 +1,41 @@
+<div class="modal" id="modal_add_content" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><i class="fas fa-plus-circle" style="color:green"></i> เพิ่มเนื้อหา</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-hover">
+                    <tr>
+                        <td>รูปภาพ</td>
+                    </tr>
+                    <tr>
+                        <td>ข้อความ</td>
+                    </tr>
+                    <tr>
+                        <td>เลือกปุ่ม</td>
+                    </tr>
+                    <tr>
+                        <td>เพิ่มฟอร์ม</td>
+                    </tr>
+                    <tr>
+                        <td>ยูทูป</td>
+                    </tr>
+                    <!-- <tr>
+                        <td>รีวิว</td>
+                    </tr>
+                    <tr>
+                        <td>เค้าดาวน์</td>
+                    </tr> -->
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
